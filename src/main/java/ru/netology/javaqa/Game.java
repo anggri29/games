338993e82ad06c63d3b257312b0873d5c2702e16
Angelina <angry.anggri@gmail.com>
2023-05-31 11:45:09 +0300

    initial commit

--- src/main/java/ru/netology/javaqa/Game.java
+++ src/main/java/ru/netology/javaqa/Game.java
@@ -1,3 +1,43 @@
 package ru.netology.javaqa;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    public List<Player> players;
+
+    public Game() {
+        players = new ArrayList<>();
+    }
+
+    public void register(Player player) {
+        players.add(player);
+    }
+
+    public Player findByName(String playerName) {
+        for (Player player : players) {
+            if (player.getName().equals(playerName)) {
+                return player;
+            }
+        }
+        return null;
+    }
+
+    public int round(String playerName1, String playerName2) throws NotRegisteredException {
+        Player player1 = findByName(playerName1);
+        Player player2 = findByName(playerName2);
+
+        if (player1 == null || player2 == null) {
+            throw new NotRegisteredException("One or more players are not registered.");
+        }
+
+        if (player1.getStrength() > player2.getStrength()) {
+            return 1; // Player 1 wins
+        } else if (player1.getStrength() < player2.getStrength()) {
+            return 2; // Player 2 wins
+        } else {
+            return 0; // Draw
+        }
+    }
+
 }
--- src/test/java/ru/netology/javaqa/GameTest.java
+++ src/test/java/ru/netology/javaqa/GameTest.java
@@ -1,3 +1,68 @@
 package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GameTest {
+
+    @Test
+    void testRound1() throws NotRegisteredException {
+        Game game = new Game();
+        Player player1 = new Player(1, "Player 1", 10);
+        Player player2 = new Player(2, "Player 2", 5);
+        game.register(player1);
+        game.register(player2);
+
+        Assertions.assertEquals(1, game.round("Player 1", "Player 2"));
+    }
+
+    @Test
+    void testRound2() throws NotRegisteredException {
+        Game game = new Game();
+        Player player1 = new Player(1, "Player 1", 5);
+        Player player2 = new Player(2, "Player 2", 10);
+        game.register(player1);
+        game.register(player2);
+
+        Assertions.assertEquals(2, game.round("Player 1", "Player 2"));
+    }
+
+    @Test
+    void testRoundDraw() throws NotRegisteredException {
+        Game game = new Game();
+        Player player1 = new Player(1, "Player 1", 10);
+        Player player2 = new Player(2, "Player 2", 10);
+        game.register(player1);
+        game.register(player2);
+
+        Assertions.assertEquals(0, game.round("Player 1", "Player 2"));
+
+    }
+
+    @Test
+    void Player1NotRegistered() {
+        Game game = new Game();
+        Player player2 = new Player(2, "Player 2", 5);
+        game.register(player2);
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> game.round("Player 1", "Player 2"));
+    }
+
+    @Test
+    void Player2NotRegistered() {
+        Game game = new Game();
+        Player player1 = new Player(1, "Player 1", 5);
+        game.register(player1);
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> game.round("Player 1", "Player 2"));
+    }
+
+    @Test
+    void BothNotRegistered() {
+        Game game = new Game();
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> game.round("Player 1", "Player 2"));
+    }
+
+
 }
